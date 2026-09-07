27091041199284a6251e8069f488a50899e6f02c
Vinbug1 <vincentwilliamsjohn9626@gmail.com>
2026-09-07 13:01:01 +0100

    update now today

--- src/routes/notificationRoutes.js
+++ src/routes/notificationRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import notificationController from '../controllers/Notificationcontroller.js';
+import notificationController from '../controllers/notificationController.js';
 import { verifyToken as authenticate } from '../middleware/auth.js';
 
 const router = express.Router();
