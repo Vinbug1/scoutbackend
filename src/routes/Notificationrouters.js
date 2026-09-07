90a7438f4eac070474d73307d81879c2485c37b2
Vinbug1 <vincentwilliamsjohn9626@gmail.com>
2026-09-07 12:22:13 +0100

    fix it up

--- src/routes/Notificationrouters.js
+++ src/routes/Notificationrouters.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import notificationController from '../controllers/Notificationcontroller';
+import notificationController from '../controllers/Notificationcontroller.js';
 import { verifyToken as authenticate } from '../middleware/auth.js';
 
 const router = express.Router();
